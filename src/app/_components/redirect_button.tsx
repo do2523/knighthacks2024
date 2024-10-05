e8928f83b9b482e07d3cf839e7a460b0841c5b27
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-10-05 19:49:44 -0400

    UI updates

--- src/app/_components/redirect_button.tsx
+++ src/app/_components/redirect_button.tsx
@@ -21,6 +21,6 @@ export default function RedirectButton({ children, href }: Readonly<{ children: 
     }
 
     return(
-        <button className="shadow-xl rounded-3xl h-[200px] w-[250px] text-2xl p-10 font-semibold" onClick={handleButton}>{children}</button>
+        <button className="shadow-lg rounded-3xl h-[200px] w-[250px] text-2xl p-10 font-semibold bg-white hover:shadow-xl" onClick={handleButton}>{children}</button>
     )
 }
--- src/app/traveler_count/page.tsx
+++ src/app/traveler_count/page.tsx
@@ -8,8 +8,8 @@ import styles from '../../styles/Shared.module.css';
 export default function TravelerCount() {
     const [option, setOption] = useState("");
 
-    return(
-        <div className="relative min-h-screen w-screen flex">
+    return (
+        <div className="relative min-h-screen w-screen flex bg-[#F0F9FF]">
             <div className="flex flex-col gap-2 items-center justify-center h-screen w-screen">
                 <div className={styles.question}>
                     Any travel buddies joining your trip?
@@ -17,8 +17,22 @@ export default function TravelerCount() {
                 <div className="flex justify-center space-x-20">
                     <Suspense><RedirectButton href="budget">Nope, it's a solo trip!</RedirectButton></Suspense>
 
-                    <button className="border-none shadow-xl rounded-3xl h-[200px] w-[250px] text-2xl p-5 font-semibold" onClick={() => setOption("accompanied")}>Yes! I'm bringing guests.</button>
-                    {option == "accompanied" && <Suspense><RedirectInput href="budget" /></Suspense>}
+                    <button className="shadow-lg rounded-3xl h-[200px] w-[250px] text-2xl p-10 font-semibold bg-white hover:shadow-xl" onClick={() => setOption("accompanied")}>
+
+                        Yes! I'm bringing guests.
+
+                        {option === "accompanied" && (
+                            <div className="mt-4 bg-black w-[210px] flex justify-center">
+                                <Suspense>
+                                    <RedirectInput href="budget" />
+                                </Suspense>
+                            </div>
+                        )}
+
+                    </button>
+
+                    {/* {option == "accompanied" && <Suspense><RedirectInput href="budget" /></Suspense>} */}
+
                 </div>
             </div>
         </div>
