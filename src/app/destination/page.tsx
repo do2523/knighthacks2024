b3b45ceaac6a9a076bfcf4eececbbf44de922b9f
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-10-05 13:16:39 -0400

    Improved Destination UI

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -1,15 +1,21 @@
 import RedirectInput from "../_components/redirect_input";
+import styles from '../../styles/Shared.module.css';
 
 export default function Destination() {
-    return(
-        <div className="relative min-h-screen w-screen flex">
-            <div>test text</div>
-            <div className="flex flex-col gap-2 items-center justify-center h-screen w-screen text-4xl font-semibold">
-                Where are you traveling to?
-                <div className="mt-8">
-                    <RedirectInput href="duration" />
+    return (
+        <div className="relative min-h-screen w-screen flex items-center justify-center">
+            <div className="flex flex-col items-center justify-center h-screen w-screen text-4xl font-semibold">
+                <div className={styles['responsive-container']}>
+                    <p className={styles['responsive-image']}>HI</p>
+                    <div className="text-center text-[#334155]">
+                        Where are you traveling to?
+                        <div className={styles['input-box']}>
+                            <RedirectInput href="duration" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
+
     );
 }
