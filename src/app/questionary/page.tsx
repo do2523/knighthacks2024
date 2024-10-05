e541ce30c87b18e87027ca3f990dc3a8f2d04d74
Mini-Chris <jenkins.l.chris@gmail.com>
2024-10-05 19:38:53 -0400

    updated questionary and traveler_count pages
    updated questionary and traveler_count pages, as well as redirect_button component to match prototype

--- src/app/questionary/page.tsx
+++ src/app/questionary/page.tsx
@@ -5,24 +5,27 @@ import styles from '../../styles/Shared.module.css';
 
 export default function Questionary() {
     return(
-        <div className="flex min-h-screen flex-col items-center justify-center">
-            <div className={styles.question}>
-                What kind of person are you?
+        <div className="relative min-h-screen w-screen flex items-center justify-center bg-[#F0F9FF]">
+            <div className="flex flex-col items-center justify-center h-screen w-screen text-4xl font-semibold">
+                    <div className={styles.question}>
+                        What kind of traveler are you?
+                    </div>
+
+                    <div className="grid grid-cols-2 md:grid-cols-4 gap-16 mt-8">
+                        <div className = "w-48 h-48">
+                            <Suspense><RedirectButton color="green" href="traveler_count">Adventure Seeker</RedirectButton></Suspense>
+                        </div>
+                        <div className = "w-48 h-48">
+                            <Suspense><RedirectButton color="cyan" href="traveler_count">Relaxation Enthusiast</RedirectButton></Suspense>
+                        </div>
+                        <div className = "w-48 h-48">
+                            <Suspense><RedirectButton color="stone" href="traveler_count">Culture Enjoyer</RedirectButton></Suspense>
+                        </div>
+                        <div className = "w-48 h-48">
+                            <Suspense><RedirectButton color="amber" href="traveler_count">The Foodie</RedirectButton></Suspense>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <div className="flex flex-row space-x-20 mt-8 h-20">
-                <div>
-                    <Suspense><RedirectButton href="traveler_count">Adventure Seeker</RedirectButton></Suspense>
-                </div>
-                <div>
-                    <Suspense><RedirectButton href="traveler_count">Relaxation Enthusiast</RedirectButton></Suspense>
-                </div>
-                <div>
-                    <Suspense><RedirectButton href="traveler_count">Culture Enjoyer</RedirectButton></Suspense>
-                </div>
-                <div>
-                    <Suspense><RedirectButton href="traveler_count">The Foodie</RedirectButton></Suspense>
-                </div>
-            </div>
-        </div>
     )
 }
--- src/app/traveler_count/page.tsx
+++ src/app/traveler_count/page.tsx
@@ -9,16 +9,24 @@ export default function TravelerCount() {
     const [option, setOption] = useState("");
 
     return(
-        <div className="relative min-h-screen w-screen flex">
-            <div className="flex flex-col gap-2 items-center justify-center h-screen w-screen">
+        <div className="relative min-h-screen w-screen flex items-center justify-center bg-sky-50">
+            <div className="flex flex-col items-center justify-center h-screen w-screen text-4xl font-semibold">
                 <div className={styles.question}>
-                    Any travel buddies joining your trip?
+                    Solo or accompanied?
                 </div>
-                <div className="flex justify-center space-x-20">
-                    <Suspense><RedirectButton href="budget">Nope, it's a solo trip!</RedirectButton></Suspense>
 
-                    <button className="border-none shadow-xl rounded-3xl h-[200px] w-[250px] text-2xl p-5 font-semibold" onClick={() => setOption("accompanied")}>Yes! I'm bringing guests.</button>
-                    {option == "accompanied" && <Suspense><RedirectInput href="budget" /></Suspense>}
+                <div className="grid grid-cols-1 md:grid-cols-2 gap-16 mt-8">
+                    <div className = "w-80 h-64">
+                        <Suspense><RedirectButton href="budget">Solo</RedirectButton></Suspense>
+                    </div>
+                    <div className = "flex flex-col justify-center items-center w-80 h-64 rounded-[2rem] bg-white shadow-lg">
+                        <p className="text-3xl text-slate-700 text-center font-bold mb-[-1rem]">Accompanied by</p>
+                        <div className="mx-4">
+                            <div className={styles['input-box']}>
+                                <Suspense><RedirectInput href="budget"></RedirectInput></Suspense>
+                            </div>
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
