fbfd33506e8b1a4bcdebd1db8322fcc2d694536f
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-10-05 03:24:05 -0400

    Initial destination UI

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -3,9 +3,12 @@ import RedirectInput from "../_components/redirect_input";
 export default function Destination() {
     return(
         <div className="relative min-h-screen w-screen flex">
-            <div className="flex flex-row gap-2 items-center justify-center h-screen w-screen">
-                Where?
-                <RedirectInput href="duration" />
+            <div>test text</div>
+            <div className="flex flex-col gap-2 items-center justify-center h-screen w-screen text-4xl font-semibold">
+                Where are you traveling to?
+                <div className="mt-8">
+                    <RedirectInput href="duration" />
+                </div>
             </div>
         </div>
     );
