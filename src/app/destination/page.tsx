42cffdeba9c14a382a749ab12b4831cd2146ec3a
do262431@gmail.com <do262431@gmail.com>
2024-10-06 09:46:09 -0400

    Add Images

--- src/app/destination/page.tsx
+++ src/app/destination/page.tsx
@@ -7,7 +7,7 @@ export default function Destination() {
         <div className="relative min-h-screen w-screen flex items-center justify-center bg-[#F0F9FF]">
             <div className="flex flex-col items-center justify-center h-screen w-screen">
                 <div className={styles['responsive-container']}>
-                    <p className={styles['responsive-image']}>HI</p>
+                <img src="/plane.png" alt="description of image" className="mr-6" />
                     <div className={styles.question}>
                         Where are you traveling to?
                         <div className={styles['input-box']}>
--- src/app/duration/page.tsx
+++ src/app/duration/page.tsx
@@ -7,7 +7,7 @@ export default function Duration() {
         <div className="relative min-h-screen w-screen flex items-center justify-center bg-[#F0F9FF]">
             <div className="flex flex-col items-center justify-center h-screen w-screen">
                 <div className={styles['responsive-container']}>
-                    <p className={styles['responsive-image']}>HI</p>
+                    <img src="/calendar.png" alt="description of image" className="mr-6" />
                     <div className={styles.question}>
                         How long is your stay?
                         <div className={styles['input-box']}>
