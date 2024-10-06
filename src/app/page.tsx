47c05cea72ec5a679d065dae0adf071c4ca8049d
Dylan Moo Hernandez <dylangmooh@gmail.com>
2024-10-05 23:24:25 -0400

    New welcome page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,36 +10,45 @@ export default async function Home() {
 
   return (
     <HydrateClient>
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-            Plan Your <span className="text-[hsl(280,100%,70%)]">Ideal</span> Trip
-          </h1>
-          <div className="flex grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 items-center justify-center m-auto">
-            <Link
-              className="flex max-w-xs flex-col gap-4 rounded-xl bg-white/10 p-4 hover:bg-white/20"
-              href="./destination"
-            >
-              <h3 className="text-2xl font-bold">Enter →</h3>
-              <div className="text-lg">
-                Prepare your ideal trip
-              </div>
-            </Link>
-          </div>
-          <div className="flex flex-col items-center gap-2">
+      <main className="bg-sky-50 pt-10">
+        <div className="mb-10 text-xl flex justify-end mr-20">
+          <Link
+            href={session ? "/api/auth/signout" : "/api/auth/signin"}
+            className="rounded-md bg-[#334155] px-10 py-4 font-semibold no-underline transition hover:bg-[#1b2534] text-white"
+          >
+            {session ? "Sign out" : "Sign in"}
+          </Link>
+        </div>
 
-            <div className="flex flex-col items-center justify-center gap-4">
-              <p className="text-center text-2xl text-white">
-                {session && <span>Logged in as {session.user?.name}</span>}
-              </p>
-              <Link
-                href={session ? "/api/auth/signout" : "/api/auth/signin"}
-                className="rounded-full bg-white/10 px-10 py-3 font-semibold no-underline transition hover:bg-white/20"
-              >
-                {session ? "Sign out" : "Sign in"}
-              </Link>
+        <div className="flex min-h-screen flex-col items-center justify-center bg-sky-50 text-[#334155]">
+          <div className="container pt-0 flex flex-col items-center justify-center gap-12 px-4 py-16">
+            <h1 className="text-2xl sm:text-3xl md:text-4xl lg:text-5xl font-semibold">Plan your next trip with</h1>
+            <h1 className="font-extrabold tracking-tight
+              text-6xl sm:text-7xl md:text-8xl lg:text-9xl xl:text-10xl">
+              Itiner<span className="text-cyan-500">AI</span>ry
+            </h1>
+            <div className="flex grid-cols-1 gap-4 sm:grid-cols-2 md:gap-8 items-center justify-center m-auto">
+            </div>
+            <div className="flex flex-col items-center gap-2">
+              <div className="text-center text-lg sm:text-xl md:text-2xl lg:text:3xl max-w-lg mx-auto mb-8">
+                Answer some questions and we'll generate a custom itinerary for you.
+              </div>
+              <div className="flex flex-col items-center justify-center gap-4">
+
+                <Link
+                  className="flex max-w-xs flex-col gap-4 rounded-lg bg-white p-4 hover:shadow-xl mb-40 shadow-lg"
+                  href="./destination"
+                >
+                  <h3 className="text-2xl font-bold">Get started →</h3>
+                </Link>
+                <p className="text-center text-2xl bg-black text-[#334155]">
+                  {session && <span>Welcome, {session.user?.name}!</span>}
+                </p>
+              </div>
             </div>
           </div>
+
+
         </div>
       </main>
     </HydrateClient>
